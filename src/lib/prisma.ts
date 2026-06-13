1fc8f6c620caec5db6552997626914f05260c7b0
Acer <kunalshinde1805@gmail.com>
2026-06-13 19:29:14 +0530

    changes commit 2

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  console.log("Initializing PrismaClient with URL:", process.env.DATABASE_URL?.replace(/:[^:]*@/, ':***@'));
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
